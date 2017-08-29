f0a7f7c2f93b7db5729cdb524f57c206cd891020
Beck Busch <beckbusch5@gmail.com>
2017-08-29 12:02:40 +1200

    filled out the tabeles and added support for switching table views

---
+++ script.js
@@ -0,0 +1,15 @@
+function loaded() {
+    document.getElementById("achived").style.display = "table";
+    document.getElementById("merit").style.display = "none";
+    document.getElementById("loading").style.display = "none";
+}
+
+function achived() {
+    document.getElementById("achived").style.display = "table";
+    document.getElementById("merit").style.display = "none";
+}
+
+function merit() {
+    document.getElementById("merit").style.display = "table";
+    document.getElementById("achived").style.display = "none";
+}
