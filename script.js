6e6ed2266cb9dbe0ff2e8a3ee668ffbf331b24c5
Beck Busch <beckbusch5@gmail.com>
2017-08-29 12:21:07 +1200

    comint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function loaded() {
-    document.getElementById("achived").style.display = "table";
-    document.getElementById("merit").style.display = "none";
-    document.getElementById("loading").style.display = "none";
-}
+// function loaded() {
+//     document.getElementById("achived").style.display = "table";
+//     document.getElementById("merit").style.display = "none";
+//     document.getElementById("loading").style.display = "none";
+// }
 
 function achived() {
     document.getElementById("achived").style.display = "table";
