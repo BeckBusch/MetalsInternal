5e4bd102cff93f6df70dc9fff24cdfdf6c2a4dac
Beck Busch <beckbusch5@gmail.com>
2017-08-30 13:29:29 +1200

    finished?

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-// function loaded() {
-//     document.getElementById("achived").style.display = "table";
-//     document.getElementById("merit").style.display = "none";
-//     document.getElementById("loading").style.display = "none";
-// }
+function loaded() {
+    document.getElementById("achived").style.display = "table";
+    document.getElementById("detail").style.display = "none";
+    document.getElementById("uses").style.display = "none";
+    document.getElementById("jewelry").style.display = "none";
+    document.getElementById("loading").style.display = "none";
+}
 
 function achived() {
     document.getElementById("achived").style.display = "table";
-    document.getElementById("merit").style.display = "none";
+    document.getElementById("detail").style.display = "none";
+    document.getElementById("uses").style.display = "none";
+    document.getElementById("jewelry").style.display = "none";
 }
 
-function merit() {
-    document.getElementById("merit").style.display = "table";
+function detail() {
     document.getElementById("achived").style.display = "none";
+    document.getElementById("detail").style.display = "table";
+    document.getElementById("uses").style.display = "none";
+    document.getElementById("jewelry").style.display = "none";
+}
+
+function uses() {
+    document.getElementById("achived").style.display = "none";
+    document.getElementById("detail").style.display = "none";
+    document.getElementById("uses").style.display = "table";
+    document.getElementById("jewelry").style.display = "none";
+}
+
+function jewelry() {
+    document.getElementById("achived").style.display = "none";
+    document.getElementById("detail").style.display = "none";
+    document.getElementById("uses").style.display = "none";
+    document.getElementById("jewelry").style.display = "table";
 }
